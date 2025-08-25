df5b15046ed4539f363167e017835a25983f1ecc
Anjan Upreti <anjanuprety787@gmail.com>
2025-08-25 19:52:04 +0545

    Updated footer with maps and contact info

--- src/Shared/Footer/Footer.tsx
+++ src/Shared/Footer/Footer.tsx
@@ -2,86 +2,67 @@ import React from "react";
 import { IoIosCall } from "react-icons/io";
 import { IoLocationSharp } from "react-icons/io5";
 import Brand from "../../Components/Brand/Brand";
-import { BiEnvelope, BiLogoLinkedin } from "react-icons/bi";
-import { FaFacebookF, FaPinterestP, FaTwitter } from "react-icons/fa";
+import { BiEnvelope } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { useLanguage } from "../../contexts/LanguageContext";
+
 const Footer: React.FC = () => {
   const { t } = useLanguage();
-  
+
   return (
     <>
       <Brand />
       <footer className="">
         {/* footer content */}
-        <div className="bg-lightBlack   ">
-          <div className="Container  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-10 2xl:grid-cols-12 gap-5 lg:gap-3 xl:gap-5 2xl:gap-[30px] pt-14 lg:pt-[100px] ">
+        <div className="bg-lightBlack">
+          <div className="Container  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-10 2xl:grid-cols-12 gap-5 lg:gap-3 xl:gap-5 2xl:gap-[30px] pt-14 lg:pt-[100px]">
             {/* Footer Content one. */}
             <div
               className="lg:mt-[-195px] lg:col-span-3 2xl:col-span-4 bg-[#385344]"
               data-aos="fade-up"
               data-aos-duration="1000"
             >
-              <div className=" py-6 md:py-7 lg:py-[50px]  px-10 lg:px-5 xl:px-8 2xl:px-9 ">
+              <div className="py-6 md:py-7 lg:py-[50px]  px-10 lg:px-5 xl:px-8 2xl:px-9">
                 <img src="/images/home-1/logo-1.png" alt="" />
-                <div className="py-8 2xl:py-[50px] ">
+                <div className="py-8 2xl:py-[50px]">
                   <h1 className="text-lg sm:text-xl md:text-[22px] leading-[38px] font-medium text-white relative font-Garamond before:w-7 before:h-[1px] before:bg-khaki before:absolute before:left-0 before:top-10">
-                    {t('footer.contact_info')}
+                    {t("footer.contact_info")}
                   </h1>
-                  <div className="space-y-4 pt-[30px]  pb-2 2xl:pb-[30px] ">
-                    <p className="flex items-center text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px] mt-2">
-                      <IoIosCall
-                        className="text-khaki w-5 h-5 mr-3 2xl:mr-4 "
-                        size={14}
-                      />
-                      +980 (1234) 567 220
-                    </p>
-                    <p className="flex items-center text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px]">
-                      <BiEnvelope
-                        className="text-khaki w-5 h-5 mr-3 2xl:mr-4 "
-                        size={14}
-                      />
-                      example@yahoo.com
-                    </p>
-                    <p className="flex items-center text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px]">
-                      <IoLocationSharp
-                        className="text-khaki w-5 h-5 mr-3 2xl:mr-4 "
-                        size={14}
-                      />
-                      102/B New Elephant Rd <br />
-                      Dhaka - 1212
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <ul className="flex space-x-3">
-                    <li className="hover-animBg group transition-all duration-300  rounded-full border border-lightGray border-opacity-75 hover:border-khaki cursor-pointer w-[37px] h-[37px] grid items-center justify-center">
-                      <Link to="#" className="">
-                        <FaFacebookF className="text-lightGray text-opacity-75 group-hover:text-white group-hover:text-slateBlue-0 w-4 h-4 " />
-                      </Link>
-                    </li>
 
-                    <li className="hover-animBg group transition-all duration-300  rounded-full border border-lightGray border-opacity-75 hover:border-khaki cursor-pointer w-[37px] h-[37px] grid items-center justify-center">
-                      <Link to="#">
-                        <FaTwitter className="text-lightGray text-opacity-75 group-hover:text-white group-hover:text-slateBlue-0 w-4 h-4 " />
-                      </Link>
-                    </li>
-                    <li className="hover-animBg group transition-all duration-300  rounded-full border border-lightGray border-opacity-75 hover:border-khaki cursor-pointer w-[37px] h-[37px] grid items-center justify-center">
-                      <Link to="#">
-                        <BiLogoLinkedin className="text-lightGray text-opacity-75 group-hover:text-white group-hover:text-slateBlue-0 w-4 h-4 " />
-                      </Link>
-                    </li>
-                    <li className="hover-animBg group transition-all duration-300  rounded-full border border-lightGray border-opacity-75 hover:border-khaki cursor-pointer w-[37px] h-[37px] grid items-center justify-center">
-                      <Link to="#">
-                        <FaPinterestP className="text-lightGray text-opacity-75 group-hover:text-white group-hover:text-slateBlue-0 w-4 h-4 " />
-                      </Link>
-                    </li>
-                  </ul>
+                  <div className="space-y-4 pt-[30px] pb-2 2xl:pb-[30px]">
+                    <a
+                      href="tel:021-464453"
+                      className="flex items-center text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px] mt-2 hover:text-white"
+                    >
+                      <IoIosCall className="text-khaki w-5 h-5 mr-3 2xl:mr-4" size={14} />
+                      <span>021-464453</span>
+                    </a>
+
+                    <a
+                      href="mailto:info@glbsl.com.np"
+                      className="flex items-center text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px] hover:text-white"
+                    >
+                      <BiEnvelope className="text-khaki w-5 h-5 mr-3 2xl:mr-4" size={14} />
+                      <span>info@glbsl.com.np</span>
+                    </a>
+
+                    <a
+                      href="https://maps.app.goo.gl/MhHVaeRkMNNVjpN19"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex items-center text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px] hover:text-white"
+                    >
+                      <IoLocationSharp className="text-khaki w-5 h-5 mr-3 2xl:mr-4" size={14} />
+                      <span>
+                        Buddhiganga-1, PuspalalChowk <br /> Morang
+                      </span>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-            {/* footer content-2 */}
 
+            {/* footer content-2 */}
             <div
               className="pt-0 pb-8 overflow-x-hidden lg:col-span-2 2xl:col-span-2 ml-2"
               data-aos="fade-up"
@@ -91,10 +72,7 @@ const Footer: React.FC = () => {
                 UseFul Links
               </h1>
               <div className="pt-[30px] pb-0 lg:py-[30px]">
-                <ul
-                  className="text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px] list-none hover:list-disc
-                 "
-                >
+                <ul className="text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px] list-none hover:list-disc">
                   <li className="hover:ml-[17px] md:hover:ml-[18px] transition-all duration-500 hover:text-khaki leading-[44px]">
                     <Link to="#">About Hotel</Link>
                   </li>
@@ -113,8 +91,8 @@ const Footer: React.FC = () => {
                 </ul>
               </div>
             </div>
-            {/* footer content-3 */}
 
+            {/* footer content-3 */}
             <div
               className="pt-0 pb-8  lg:col-span-3 2xl:col-span-3"
               data-aos="fade-up"
@@ -132,32 +110,45 @@ const Footer: React.FC = () => {
                 <img src="/images/home-1/gallery-6.jpg" alt="" />
               </div>
             </div>
-            {/* footer content-4 */}
 
+            {/* footer content-4 */}
             <div
-              className="pt-0 pb-8 overflow-x-hidden lg:col-span-2 2xl:col-span-3"
+              className="pt-0 pb-8 overflow-x-hidden lg:col-span-3 2xl:col-span-3"
               data-aos="fade-up"
               data-aos-duration="1000"
             >
               <h1 className="text-lg sm:text-xl md:text-[22px] leading-[38px] font-medium text-white relative font-Garamond before:w-7 before:h-[1px] before:bg-khaki before:absolute before:left-0 before:top-10 uppercase ">
-                NEWSLETTER
+                LOCATE US:
               </h1>
-              <div className="space-y-4 py-[30px]">
-                <p className="text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px]">
-                  Subscribe our Newsletter
-                </p>
-                <form action="" className="space-y-4">
-                  <input
-                    type="email"
-                    placeholder="Enter Email"
-                    className="placeholder:text-gray placeholder:font-Lora placeholder:font-normal placeholder:text-[15px] text-center h-[56px] w-full px-10 placeholder:leading-[26px] text-[15px] text-gray border-none outline-none focus:outline-none focus:border-khaki focus:ring-0"
-                    required
-                  />
-                  <button className="btn-subscribe">Subscribe Now</button>
-                </form>
+              <div className="space-y-4 py-[36px]">
+                {/* <p className="text-lightGray font-Lora font-normal text-sm sm:text-base leading-[26px]">Open with google maps</p> */}
+
+                {/* Map preview (approx 300x200) — opens maps.app link when clicked */}
+                <div className="w-full flex items-center">
+                  <div className="rounded border border-[#eee] overflow-hidden w-[250px] sm:w-[300px] lg:w-full">
+                    <iframe
+                      title="GLBSL Location"
+                      src="https://www.google.com/maps?q=Buddhiganga-1%20PuspalalChowk%20Morang%20Nepal&output=embed"
+                      width="100%"
+                      height="200"
+                      style={{ border: 0, display: "block" }}
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="ml-4 flex-1">
+                    <a
+                      href="https://maps.app.goo.gl/MhHVaeRkMNNVjpN19"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-block text-khaki font-Lora text-sm hover:underline"
+                    >
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+
           <div className="text-center py-5 2xl:py-7 bg-[#161616] text-sm md:text-base text-lightGray font-Lora font-normal">
             {` © ${new Date().getFullYear()} , Royella. All Rights Reserved.`}
           </div>
