1e48501b370081caa3db80cea8011a7f75b7d85d
Anjan Upreti <anjanuprety787@gmail.com>
2025-08-28 18:28:03 +0545

    Steps 3 & 4: Remove hotel pages and PAGE section from navbar
    Step 3 - Remove hotel-related pages:
    - Deleted Room.tsx, FindRoom.tsx, RoomDetails.tsx (hotel room pages)
    - Deleted Blog.tsx, BlogDetails.tsx, BlogSideBar.tsx (blog functionality)
    - Deleted Pricing.tsx (hotel pricing page)
    - Removed corresponding routes from Router.tsx:
      * /room, /find_room, /room_details
      * /blog, /blog_details
      * /pricing
    
    Step 4 - Clean up navbar:
    - Removed PAGE section completely (contained duplicate links)
    - Removed blog section from navbar (since blog pages deleted)
    - Navbar now has cleaner structure without redundant menu items
    
    Template conversion complete - all hotel-specific functionality removed

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -17,16 +17,10 @@ import RemittanceServicesPage from "../Pages/Services/services/RemittanceService
 import MemberWelfareServicesPage from "../Pages/Services/services/MemberWelfare/MemberWelfareServicesPage";
 
 // Inner pages
-import Room from "../Pages/InnerPage/Room";
-import FindRoom from "../Pages/InnerPage/FindRoom";
-import RoomDetails from "../Pages/InnerPage/RoomDetails";
 import ReportsPage from "../Pages/InnerPage/ReportsPage";
 import Services from "../Pages/InnerPage/Services";
 import ServiceDetails from "../Pages/InnerPage/ServiceDetails";
 import Team from "../Pages/InnerPage/Team";
-import Pricing from "../Pages/InnerPage/Pricing";
-import Blog from "../Pages/InnerPage/Blog";
-import BlogDetails from "../Pages/InnerPage/BlogDetails";
 import Contact from "../Pages/InnerPage/Contact";
 import ErrorPage from "../Shared/ErrorPage/ErrorPage";
 
@@ -79,18 +73,6 @@ const router = createBrowserRouter([
       {
         path: "/branches",
         element: <BranchesPage />,
-      },
-      {
-        path: "/room",
-        element: <Room />,
-      },
-      {
-        path: "/find_room",
-        element: <FindRoom />,
-      },
-      {
-        path: "/room_details",
-        element: <RoomDetails />,
       },
       {
         path: "/reports",
@@ -151,18 +133,6 @@ const router = createBrowserRouter([
       {
         path: "/our_team",
         element: <Team />,
-      },
-      {
-        path: "/pricing",
-        element: <Pricing />,
-      },
-      {
-        path: "/blog",
-        element: <Blog />,
-      },
-      {
-        path: "/blog_details",
-        element: <BlogDetails />,
       },
       {
         path: "/contact",
